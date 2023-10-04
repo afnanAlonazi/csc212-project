d40f9be2201b72ff44bb778184d1e37a9f4ed55f
157nouraalhumaid <146860215+157nouraalhumaid@users.noreply.github.com>
2023-10-04 21:06:25 +0300

    Update LinkedList.java
    method delete

--- PAV1/src/LinkedList.java
+++ PAV1/src/LinkedList.java
@@ -45,5 +45,26 @@ public class LinkedList<T> {
 		Node <T> post= null;
 		
 	}
+	public void deleteContact(String NameOrPhone) {
+		if(head==null)
+			return ;
+		
+		if(head.next==null) {
+			head=head.next;
+			System.out.println("contact deleted successfully");
+		}
+                else{
+                    Node<T> current = head;
+                    while(current.next != null) {
+                      if (((Contact) current.next.data).getContactName().equals(NameOrPhone)||((Contact) current.next.data).getPhoneNumber().equals(NameOrPhone)) {
+                          current.next = current.next.next;
+                          System.out.println("Contact deleted successfully");
+                          return;
+                          }//end if
+                         current = current.next;
+                          }//end of while
+                    }//end of else       
+       System.out.println("Contact can not found!!");
+	}
 		
 }
